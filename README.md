ff0c58d4f2970fbf858c92420c4710a9518df2e4
Alexander Brooks <alexanderlawbrooks@gmail.com>
2022-08-10 11:23:51 -0700

    built out initial folder structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Data Structures & Algorithms
+### My Coding Solutions for Different LeetCode Problems
---
+++ Tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tree
+//
+//  Created by Alexander Brooks on 8/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

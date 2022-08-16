52b32af3e4f11fb17abe0283d70d659562a9e5d2
Alexander Brooks <alexanderlawbrooks@gmail.com>
2022-08-16 11:58:02 -0700

    ransom note

---
+++ String/Solution383.cpp
@@ -0,0 +1,29 @@
+// Problem 383: Ransom Note
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using std::string, std::vector, std::unordered_map;
+
+class Solution383 {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> magazine_count(128);
+        
+        for (int i = 0; i < magazine.size(); ++i) {
+            int letter = (int) magazine[i];
+            magazine_count[letter]++;
+        }
+        
+        for (int i = 0; i < ransomNote.size(); ++i) {
+            int letter = (int) ransomNote[i];
+            if (magazine_count[letter] > 0) {
+                magazine_count[letter]--;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
